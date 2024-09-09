5937ada259aa833f14dfee413b2705dd64c63e0f
VanitasCaesar1 <chakravarthi8a2ss@gmail.com>
2024-09-09 23:22:46 +0530

    one

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import {z, defineCollection } from 'astro:content'
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const bookCollection = defineCollection({
+    type:"data",
+    schema: z.object({
+        author: z.string(),
+        awards: z.array(z.string()),
+        bookId: z.string(),
+        characters: z.string(),
+        description: z.string().max(40),
+        firstPublishDate: z.date(),
+        genres: z.array(z.string()),
+        isbn: z.string(),
+        language: z.string(),
+        pages: z.string(),
+        publishDate: z.date(),
+        publisher: z.string(),
+        series: z.string(),
+        title: z.string()
+      
+    })
 });
-
-export const collections = { blog };
+export const collection = {
+    'books' : bookCollection,
+}
