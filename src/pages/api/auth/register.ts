c151337cb6d47c672aaf0868bda3af42c7bca30b
VanitasCaesar1 <chakravarthi8a2ss@gmail.com>
2024-09-11 18:32:24 +0530

    added supabase login

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,28 @@
+// With `output: 'hybrid'` configured:
+// export const prerender = false;
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+  const cnfpwd = formData.get("confirmPassword")?.toString();
+
+  if (!email || !password) {
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    
+    redirect('/register')
+
+  }
+
+  return redirect("/login");
+};
